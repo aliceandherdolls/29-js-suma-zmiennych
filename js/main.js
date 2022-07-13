104cda567c14b5282dc5bf6357fd0037bf4ad0c4
aliceandherdolls <alicjagorkiewicz92@gmail.com>
2022-07-13 15:03:31 +0200

    sum

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let firstNumber = 6;
+
+let secondNumber = 7;
+
+function sum(par1,par2){
+    let result = par1+ par2;   
+    return result;
+}
+
+
+console.log(sum(firstNumber, secondNumber));
+
+
